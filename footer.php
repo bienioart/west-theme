5938fca7c7f1cf9959cc4807bf83c10d8a4baf2a
Mateusz Bien <mateuszbien@yahoo.com>
2021-02-05 12:47:06 +0100

    template home end with footer

--- footer.php
+++ footer.php
@@ -17,6 +17,61 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="wrapper-footer">
 
+        <div class="footer-testimonial">
+            <div class="container">
+                <div class="testimonial">
+                    <div class="stars">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>    
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    </div>
+                    <div class="quote">
+                        "Amazing customer service and very good price will be definitely getting another one throught these guys when needed so much help thankyou"
+                    </div>
+                   <div class="author">
+                        Damon, facebook review
+                   </div>
+                </div>
+            </div>
+        </div>
+       
+            <div class="container">
+                <div class="footer-contact">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="title">Want to know more?</div>
+                            <div class="desc">To find out more about our 
+                            services please contact us using any of the methods below, or alternatively fill in the contact 
+                            form and a representative will be in touch as soon as possible.</div>
+                            <div class="contact-info">
+                                <div class="call">
+                                    <a href="tel:1800-927-831"><i class="fa fa-phone" aria-hidden="true"></i>Call today 1800 927 831</a>
+                                </div>
+                                <div class="email">
+                                    <a href="mailto:info@westcoastwaste.com.au"><i class="fa fa-envelope" aria-hidden="true"></i>Email info@westcoastwaste.com.au</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="contact-form-holder">
+                                <?php echo do_shortcode('[contact-form-7 id="55" title="Footer contact form"]'); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <div class="copyright">
+            <div class="container">
+                <div class="text">
+                 © Copyright West Coast Waste 2020   <span>//</span>  Term &  Conditions  <span>//</span>  Website by Mateusz Bień
+                </div>
+            </div>
+
+        </div>
+
 </div><!-- wrapper end -->
 
 </div><!-- #page we need this extra closing tag here -->
--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -24,7 +24,8 @@ if ( is_front_page() ) {
 			<div class="col-xl-7 col-md-12">
 				<div class="skip-bin-info">
 					<div class="overlay-image-arrow">
-						<img src="<?php echo get_template_directory_uri(); ?>/img/home-hero-arrow-up.png" alt="arrow-up">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/home-hero-arrow-up.png"
+							alt="arrow-up">
 					</div>
 					<div class="title">
 						<i class="fa fa-arrow-circle-up" aria-hidden="true"></i> Skip Bin instant quote
@@ -32,20 +33,21 @@ if ( is_front_page() ) {
 					<div class="desc">
 						Simply type your suburb above and you will be provided with an instant price across our range
 					</div>
-				</div>	
+				</div>
 			</div>
 			<div class="col-lg-1"></div>
 			<div class="col-xl-4 col-lg-12">
-			<div class="home-hero">
-									<img src="<?php echo get_template_directory_uri(); ?>/img/home-hero-right-info.png" alt="home-hero" width="100%">
-									<div class="seven-day-info">	
-										<div class="title">7-Day skip bin hire direct to your home covering Perth to Albany</div>
-									</div>
-							</div>
+				<div class="home-hero">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/home-hero-right-info.png" alt="home-hero"
+						width="100%">
+					<div class="seven-day-info">
+						<div class="title">7-Day skip bin hire direct to your home covering Perth to Albany</div>
+					</div>
+				</div>
 			</div>
-		
-		
-		
+
+
+
 		</div>
 
 		<div class="home-hero-slider">
@@ -56,7 +58,7 @@ if ( is_front_page() ) {
 						</div>
 						<div class="entry-content">
 							<div class="icon">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/home-trailer.png" alt="">
+								<img src="<?php echo get_template_directory_uri(); ?>/img/home-trailer.png" alt="">
 							</div>
 							<div class="info">
 								We deliver the right skip bin for your residential and commercial projects.
@@ -87,7 +89,8 @@ if ( is_front_page() ) {
 				</div>
 				<div class="entry-content">
 					<div class="title">15 Day Storm Special on 9m3 Skip Bins</div>
-					<div class="desc">BIN SERVICES - Perth, Fermantle, Kwianana, Rockingham, Mandurah, Pinjarra, Waroona, Harvey, Australid, Bunbury, Busselton, Margaret River</div>
+					<div class="desc">BIN SERVICES - Perth, Fermantle, Kwianana, Rockingham, Mandurah, Pinjarra,
+						Waroona, Harvey, Australid, Bunbury, Busselton, Margaret River</div>
 					<a href="#" class="button">FIND OUT MORE INFORMATION</a>
 				</div>
 			</div>
@@ -96,58 +99,253 @@ if ( is_front_page() ) {
 
 	<div class="home-products-holder">
 		<div class="container">
-			<h2><i class="fa fa-arrow-circle-down" aria-hidden="true"></i> Select the right skip bin for your project</h2>
-			<div class="home-products">
-				<div class="home-product">
-					<div class="row">
-						<div class="col-lg-4">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/6m3-Skip-Bin.png" alt="product-placeholder" width="100%">
-						</div>
-						<div class="col-lg-4">
-							<div class="title">4 Cubic Metre Skip</div>
-							<ul>
-								<li>Garden & Household clean-up</li>
-								<li>Heavy Loads (dirt, concrete, bricks or rubble)</li>
-								<li>Ideal for narrow alleys or small streets</li>
-								<li>Building Sites</li>
-							</ul>
-							
-						</div>
-						<div class="col-lg-4">
+			<h2><i class="fa fa-arrow-circle-down" aria-hidden="true"></i> Select the right skip bin for your project
+			</h2>
+
+
+			<div class="home-product">
+				<div class="row">
+					<div class="col-xl-4 col-lg-5 col-md-12">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/6m3-Skip-Bin.png"
+							alt="product-placeholder" width="100%">
+					</div>
+					<div class="col-xl-4 col-lg-4 col-md-12">
+						<div class="title">4 Cubic Metre Skip</div>
+						<ul>
+							<li>Garden & Household clean-up</li>
+							<li>Heavy Loads (dirt, concrete, bricks or rubble)</li>
+							<li>Ideal for narrow alleys or small streets</li>
+							<li>Building Sites</li>
+						</ul>
+
+					</div>
+					<div class="col-xl-4 col-lg-2 col-md-12 ">
+						<div class="col-xl-12 col-lg-3 col-md-12">
 							<div class="approx-circles">
 								<div class="row">
-									<div class="col-lg-6">
+									<div class="col-xl-6 col-lg-4 col-md-4  col-sm-6">
 										<div class="circle">
 											<div class="title">Approx x4 trailers</div>
 											<div class="icon">
-												<img src="<?php echo get_template_directory_uri(); ?>/img/home-trailer.png" alt="">
+												<img src="<?php echo get_template_directory_uri(); ?>/img/home-trailer.png"
+													alt="">
 											</div>
 										</div>
 									</div>
-									<div class="col-lg-6 align-middle">	
+									<div class="col-xl-6 col-lg-4 col-md-4  col-sm-6">
 										<div class="circle ">
 											<div class="title">Approx x4 trailers</div>
 											<div class="icon">
-												<img src="<?php echo get_template_directory_uri(); ?>/img/home-product.png" alt="">
+												<img src="<?php echo get_template_directory_uri(); ?>/img/home-product.png"
+													alt="">
 											</div>
 										</div>
 									</div>
 								</div>
-								
 							</div>
 						</div>
-						<div class="col-lg-4">
-						</div>
+
 					</div>
-
-					
-
 					<div class="abs-holder-button">Get a quote for this skip bin</div>
 				</div>
 			</div>
+
+			<div class="home-product">
+				<div class="row">
+					<div class="col-xl-4 col-lg-5 col-md-12">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/6m3-Skip-Bin.png"
+							alt="product-placeholder" width="100%">
+					</div>
+					<div class="col-xl-4 col-lg-4 col-md-12">
+						<div class="title">4 Cubic Metre Skip</div>
+						<ul>
+							<li>Garden & Household clean-up</li>
+							<li>Heavy Loads (dirt, concrete, bricks or rubble)</li>
+							<li>Ideal for narrow alleys or small streets</li>
+							<li>Building Sites</li>
+						</ul>
+
+					</div>
+					<div class="col-xl-4 col-lg-2 col-md-12 ">
+						<div class="col-xl-12 col-lg-3 col-md-12">
+							<div class="approx-circles">
+								<div class="row">
+									<div class="col-xl-6 col-lg-4 col-md-4  col-sm-6">
+										<div class="circle">
+											<div class="title">Approx x4 trailers</div>
+											<div class="icon">
+												<img src="<?php echo get_template_directory_uri(); ?>/img/home-trailer.png"
+													alt="">
+											</div>
+										</div>
+									</div>
+									<div class="col-xl-6 col-lg-4 col-md-4  col-sm-6">
+										<div class="circle ">
+											<div class="title">Approx x4 trailers</div>
+											<div class="icon">
+												<img src="<?php echo get_template_directory_uri(); ?>/img/home-product.png"
+													alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<div class="abs-holder-button">Get a quote for this skip bin</div>
+				</div>
+			</div>
+
+			<div class="home-product">
+				<div class="row">
+					<div class="col-xl-4 col-lg-5 col-md-12">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/6m3-Skip-Bin.png"
+							alt="product-placeholder" width="100%">
+					</div>
+					<div class="col-xl-4 col-lg-4 col-md-12">
+						<div class="title">4 Cubic Metre Skip</div>
+						<ul>
+							<li>Garden & Household clean-up</li>
+							<li>Heavy Loads (dirt, concrete, bricks or rubble)</li>
+							<li>Ideal for narrow alleys or small streets</li>
+							<li>Building Sites</li>
+						</ul>
+
+					</div>
+					<div class="col-xl-4 col-lg-2 col-md-12 ">
+						<div class="col-xl-12 col-lg-3 col-md-12">
+							<div class="approx-circles">
+								<div class="row">
+									<div class="col-xl-6 col-lg-4 col-md-4  col-sm-6">
+										<div class="circle">
+											<div class="title">Approx x4 trailers</div>
+											<div class="icon">
+												<img src="<?php echo get_template_directory_uri(); ?>/img/home-trailer.png"
+													alt="">
+											</div>
+										</div>
+									</div>
+									<div class="col-xl-6 col-lg-4 col-md-4  col-sm-6">
+										<div class="circle ">
+											<div class="title">Approx x4 trailers</div>
+											<div class="icon">
+												<img src="<?php echo get_template_directory_uri(); ?>/img/home-product.png"
+													alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<div class="abs-holder-button">Get a quote for this skip bin</div>
+				</div>
+			</div>
+
+
+			<div class="home-product">
+				<div class="row">
+					<div class="col-xl-4 col-lg-5 col-md-12">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/6m3-Skip-Bin.png"
+							alt="product-placeholder" width="100%">
+					</div>
+					<div class="col-xl-4 col-lg-4 col-md-12">
+						<div class="title">4 Cubic Metre Skip</div>
+						<ul>
+							<li>Garden & Household clean-up</li>
+							<li>Heavy Loads (dirt, concrete, bricks or rubble)</li>
+							<li>Ideal for narrow alleys or small streets</li>
+							<li>Building Sites</li>
+						</ul>
+
+					</div>
+					<div class="col-xl-4 col-lg-2 col-md-12 ">
+						<div class="col-xl-12 col-lg-3 col-md-12">
+							<div class="approx-circles">
+								<div class="row">
+									<div class="col-xl-6 col-lg-4 col-md-4  col-sm-6">
+										<div class="circle">
+											<div class="title">Approx x4 trailers</div>
+											<div class="icon">
+												<img src="<?php echo get_template_directory_uri(); ?>/img/home-trailer.png"
+													alt="">
+											</div>
+										</div>
+									</div>
+									<div class="col-xl-6 col-lg-4 col-md-4  col-sm-6">
+										<div class="circle ">
+											<div class="title">Approx x4 trailers</div>
+											<div class="icon">
+												<img src="<?php echo get_template_directory_uri(); ?>/img/home-product.png"
+													alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<div class="abs-holder-button">Get a quote for this skip bin</div>
+				</div>
+			</div>
+
+
 		</div>
 	</div>
 
+
+	<div class="home-following-waste">
+		<div class="container">
+			<div class="title-holder top">
+				<h2>We accept the following waste</h2>
+				<div class="button-holder">
+					<a href="#" class="button">read the full list of acceptable waste</a>
+				</div>
+			</div>
+
+
+			<div class="row">
+				<div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 text-center">
+					<div class="circle">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/home-we-accept-1.png" alt="">
+						<div class="title">Commercial Waste</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 text-center">
+					<div class="circle">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/home-we-accept-2.png" alt="">
+						<div class="title">Building Waste</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 text-center">
+					<div class="circle">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/home-we-accept-3.png" alt="">
+						<div class="title">Green Waste</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 text-center">
+					<div class="circle">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/home-we-accept-4.png" alt="">
+						<div class="title">Household Waste</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="title-holder bottom">
+				<div class="button-holder">
+					<a href="#" class="button">read the full list of acceptable waste</a>
+				</div>
+			</div>
+		</div>
+
+
+
+
+
+	</div>
 </div>
 
 
